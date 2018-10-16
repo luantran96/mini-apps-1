8a97dd83d9840576c4b86f76337d617bc19448e7
Luan Tran <luan.phuoc.tran.96@gmail.com>
2018-10-16 16:19:50 -0700

    Data sent and received correctly

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,8 +1,25 @@
 
+// function postData(url = ``, data = {}) {
+//   // Default options are marked with *
+//     return fetch(url, {
+//         method: "POST", // *GET, POST, PUT, DELETE, etc.
+//         mode: "cors", // no-cors, cors, *same-origin
+//         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//         credentials: "same-origin", // include, same-origin, *omit
+//         headers: {
+//             "Content-Type": "application/json; charset=utf-8",
+//             // "Content-Type": "application/x-www-form-urlencoded",
+//         },
+//         redirect: "follow", // manual, *follow, error
+//         referrer: "no-referrer", // no-referrer, *client
+//         body: JSON.stringify(data), // body data type must match "Content-Type" header
+//     })
+//     .then(response => response.json()); // parses response to JSON
+// }
 
-$('button').on('click', () =>{
-	var textInput = $('input').val();
-	alert(textInput);
+
+$('#submit').on('click', () => {
+
+	console.log($('input').val());
 
 });
-
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,20 +3,73 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 4568;
 const fs = require('fs');
-
-
+const _ = require('underscore');
 
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('client'));
+app.set('views', './views');
+app.set('view engine', 'pug');
 
-app.post('/create', (req,res) => {
+var generateCSV = (JSONdata) => {
+	var result = '';
+	var keys = [];
 
-	console.log('IM IN /CREATE');
+	for(var key in JSONdata) {
 
+		if (key !== 'children') {
+			result += key + ',';
+			keys.push(key);
+
+		} else {
+			result += '\n';
+		}
+
+	}
+
+
+	var generateCSV = (curPerson) => {
+
+	//result += `${curPerson.firstName},${curPerson.lastName},${curPerson.county},${curPerson.city},${curPerson.role},${curPerson.sales}\n`;
+
+	var rowData = [];
+
+	for (var key in curPerson) {
+		if(key !== 'children') {
+			rowData.push(curPerson[key]);
+			//console.log(rowData);
+		} else {
+			rowData.push('\n');
+			result += rowData.join();
+			//console.log(rowData.join());	
+		}
+	}
+
+		if(curPerson.children) {
+			curPerson.children.forEach((child,idx) => {
+				generateCSV(child);
+			});
+		}
+	};
+
+	generateCSV(JSONdata);
+	return result;
+};
+
+app.post('/', (req,res) => {
+	console.log('\n\n');
+	var JSONdata = JSON.parse(req.body.textValue);
+
+	var CSVreport = generateCSV(JSONdata);
+
+	console.log('result after generateCSV: \n ', CSVreport);
+
+	res.sendFile(__dirname + '/client/index.html');
+	res.render('index.ejs',{CSVreport});
 });
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 

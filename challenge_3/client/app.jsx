6d3e33370954098311aa5915d150031425df357c
Luan Tran <luan.phuoc.tran.96@gmail.com>
2018-10-17 16:00:21 -0700

    Input are received correctly

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -51,38 +51,58 @@ class Form1 extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = this.props.states;
+		this.state = {};
 
-		this.state.name = 'Luan';
-		this.state.email = 'abc@mail.com';
-		this.state.password = 'Luan';
+		this.state.name = '';
+		this.state.email = '';
+		this.state.password = '';
 
 
-
+		this.handleChange = this.handleChange.bind(this);
 		this.moveToForm2 = this.moveToForm2.bind(this);
 	};
 
 
 	moveToForm2 () {
-		ReactDOM.render(<Form2 states = {this.state} />, document.getElementById('app'));
+
+		// this.setState({
+		// 	name: document.getElementById('name').value(),
+		// 	email: document.getElementById('email').value(),
+		// 	password: document.getElementById('password').value()
+		// });
+
+		ReactDOM.render(<Form2 states = {this.state} handleChange= {this.handleChange} />, document.getElementById('app'));
 	};
 
+
+	 handleChange(target) {
+	 	let id = target.id;
+	 	let val = target.value;
+
+	 	console.log('target.id:', id);
+	 	console.log('target.value:', val);
+
+		    this.setState({
+		    [id] : val
+			});
+	 }
 
 	render() {
 
 		return (
+
 			<div>
 			<div>
-			<textarea placeholder='Name'>
-			</textarea>
+			<input type='text' id='name' placeholder='Name' onChange = {(e) => this.handleChange(e.target)} >
+			</input>
 			</div>
 			<div>
-			<textarea placeholder='Email'>
-			</textarea>
+			<input type='text' id='email' placeholder='Email' onChange = {(e) => this.handleChange(e.target)}>
+			</input>
 			</div>
 			<div>
-			<textarea placeholder='Password'>
-			</textarea>
+			<input type='password' id='password' placeholder='Password' onChange = {(e) => this.handleChange(e.target)}>
+			</input>
 			</div>
 				<button onClick = {this.moveToForm2}>
 				 Next
@@ -91,8 +111,6 @@ class Form1 extends React.Component {
 		)
 	};
 }
-
-
 
 class Form2 extends React.Component {
 
@@ -103,19 +121,31 @@ class Form2 extends React.Component {
 
 		console.log('states in Form2 props: ',this.state);
 
-		this.state.address = {};
-		this.state.address.line1 = '123';
-		this.state.address.line2 = 'red St';
-		this.state.address.city = 'Daly City';
-		this.state.address.state = 'CA';
-		this.state.address.zip = '95014';
-		this.state.phoneNum = '12345';
+		this.state.line1 = '';
+		this.state.line2 = '';
+		this.state.city = '';
+		this.state.state = '';
+		this.state.zip = '';
+		this.state.phoneNum = '';
 
 
 		this.moveToForm1 = this.moveToForm1.bind(this);
 		this.moveToForm3 = this.moveToForm3.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 
 	}
+
+	 handleChange(target) {
+	 	let id = target.id;
+	 	let val = target.value;
+
+	 	console.log('target.id:', id);
+	 	console.log('target.value:', val);
+
+		    this.setState({
+		    [id] : val
+			});
+	 }
 
 	moveToForm1 () {
 		ReactDOM.render(<Form1 states = {this.state}/>, document.getElementById('app'));
@@ -131,30 +161,30 @@ class Form2 extends React.Component {
 
 			<div>
 				<div>
-					<textarea placeholder='Line 1'>
-					</textarea>
+					<input type='text' id='line1' placeholder='Line 1' onChange = {(e) => this.handleChange(e.target)}>
+					</input>
 				</div>
 				<div>
-					<textarea placeholder='Line 2'>
-					</textarea>
+					<input type='text' id='line2' placeholder='Line 2' onChange = {(e) => this.handleChange(e.target)}>
+					</input>
 				</div>
 				<div>
-					<textarea placeholder='City'>
-					</textarea>
+					<input type='text' id='city' placeholder='City' onChange = {(e) => this.handleChange(e.target)}>
+					</input>
 				</div>
 				<div>
-					<textarea placeholder='State'>
-					</textarea>
-				</div>
-
-				<div>
-					<textarea placeholder='ZIP'>
-					</textarea>
+					<input type='text' id='state' placeholder='State' onChange = {(e) => this.handleChange(e.target)}>
+					</input>
 				</div>
 
 				<div>
-					<textarea placeholder='Phone Number'>
-					</textarea>
+					<input type='text' id='zip' placeholder='ZIP' onChange = {(e) => this.handleChange(e.target)}>
+					</input>
+				</div>
+
+				<div>
+					<input type='text' id='phoneNum' placeholder='Phone Number' onChange = {(e) => this.handleChange(e.target)}>
+					</input>
 				</div>
 
 				<button onClick = {this.moveToForm1}>
@@ -184,8 +214,21 @@ class Form3 extends React.Component {
 		console.log('states in Form3:', this.state);
 
 		this.moveToForm2 = this.moveToForm2.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+
 	};
 
+	 handleChange(target) {
+	 	let id = target.id;
+	 	let val = target.value;
+
+	 	console.log('target.id:', id);
+	 	console.log('target.value:', val);
+
+		    this.setState({
+		    [id] : val
+			});
+	 }
 
 	moveToForm2 () {
 		ReactDOM.render(<Form2 states = {this.state} />, document.getElementById('app'));
@@ -197,20 +240,20 @@ class Form3 extends React.Component {
 
 			<div>
 				<div>
-					<textarea placeholder='card number'>
-					</textarea>
+					<input type='text' id='cardNum' placeholder='card number' onChange = {(e) => this.handleChange(e.target)}>
+					</input>
 				</div>
 				<div>
-					<textarea placeholder='expired date'>
-					</textarea>
+					<input type='text' id='expiredDate' placeholder='expired date' onChange = {(e) => this.handleChange(e.target)}>
+					</input>
 				</div>
 				<div>
-					<textarea placeholder='CVV'>
-					</textarea>
+					<input type='text' id='CVV' placeholder='CVV' onChange = {(e) => this.handleChange(e.target)}>
+					</input>
 				</div>
 				<div>
-					<textarea placeholder='billing Zip Code'>
-					</textarea>
+					<input type='text' id='billingZipCode' placeholder='billing Zip Code' onChange = {(e) => this.handleChange(e.target)}>
+					</input>
 				</div>
 
 				<button onClick = {this.moveToForm2}>
